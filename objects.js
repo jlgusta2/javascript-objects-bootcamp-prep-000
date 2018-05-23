1b2504572a0cba0cc9dbf26620e03a855ed0a297
jennifer gustavson <jengustavson@gmail.com>
2018-05-23 22:09:57 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,11 @@
-var playlist = {Nirvana: "All Apologies"};
+var playlist = {artist: "song"};
 
 function updatePlaylist(playlist, artist, song){
   playlist[artist] = song;
   return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist
   
 }
